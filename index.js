816807c0ca61f099c88adc5de18d3eb2da3d4f3d
davidsackett14 <david@dsihvac.com>
2021-03-10 17:47:42 -0700

    add index.js file for adding questions to

---
+++ index.js
@@ -0,0 +1,20 @@
+// array of questions for user
+const questions = [{
+    type
+    name = 'title',
+
+}
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
